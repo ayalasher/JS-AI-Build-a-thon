a480fe66acceed978234e6215c811e932150834f
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-06-11 12:23:08 +0300

    Connect to Azure AI Foundry

---
+++ ai-foundry.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import ModelClient from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+const client = new ModelClient(
+  process.env.AZURE_INFERENCE_SDK_ENDPOINT ??
+    "https://maxwe-mbrowzk9-eastus2.services.ai.azure.com/models",
+  new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "YOUR_KEY_HERE")
+);
+
+var messages = [
+  { role: "developer", content: "You are an helpful assistant" },
+  { role: "user", content: "What are the 3 newton laws of motion ? " },
+];
+
+var response = await client.path("chat/completions").post({
+  body: {
+    messages: messages,
+    max_completion_tokens: 800,
+    temperature: 1,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    model: "gpt-4.1",
+  },
+});
+
+// Extract just the AI's response content
+console.log(response.body.choices[0].message.content);
